ccc58f7377488cc6b1be2e5d8fad3e57d8f1da94
osvaldimar <osvaldimar1@gmail.com>
2016-07-25 17:05:19 -0300

    hobby

--- PrjRedeSocial/src/com/iliketo/model/Hobby.java
+++ PrjRedeSocial/src/com/iliketo/model/Hobby.java
@@ -23,26 +23,46 @@ public class Hobby extends ContentILiketo {
 	@ColumnILiketo(name = "name_category")
 	private String nameCategory;
 	
+	@ColumnILiketo(name = "super_category")
+	private String superCategory;
+	
 	@ColumnILiketo(name = "fk_user_id")
 	private String idMember;
 	
-	@ColumnILiketo(name = "country")
-	private String country;
+	@ColumnILiketo(name = "idioma")
+	private String idiom;
+
 	
+	//notificacoes
+	@ColumnILiketo(name = "notific_item")
+	private String notificItem;
+	
+	@ColumnILiketo(name = "notific_video")
+	private String notificVideo;
+	
+	@ColumnILiketo(name = "notific_event")
+	private String notificEvent;
+	
+	@ColumnILiketo(name = "notific_announce")
+	private String notificAnnounce;
+	
+	@ColumnILiketo(name = "notific_topic")
+	private String notificTopic;
+	
+	@ColumnILiketo(name = "notific_comment")
+	private String notificComment;
+		
 	public Hobby(){
 		
 	}
-	
 
 	public String getIdHobby() {
 		return idHobby;
 	}
 
-
 	public void setIdHobby(String idHobby) {
 		this.idHobby = idHobby;
 	}
-
 
 	public String getIdCategory() {
 		return idCategory;
@@ -60,6 +80,14 @@ public class Hobby extends ContentILiketo {
 		this.nameCategory = nameCategory;
 	}
 
+	public String getSuperCategory() {
+		return superCategory;
+	}
+
+	public void setSuperCategory(String superCategory) {
+		this.superCategory = superCategory;
+	}
+
 	public String getIdMember() {
 		return idMember;
 	}
@@ -68,12 +96,60 @@ public class Hobby extends ContentILiketo {
 		this.idMember = idMember;
 	}
 
-	public String getCountry() {
-		return country;
+	public String getIdiom() {
+		return idiom;
 	}
 
-	public void setCountry(String country) {
-		this.country = country;
+	public void setIdiom(String idiom) {
+		this.idiom = idiom;
+	}
+
+	public String getNotificItem() {
+		return notificItem;
+	}
+
+	public void setNotificItem(String notificItem) {
+		this.notificItem = notificItem;
+	}
+
+	public String getNotificVideo() {
+		return notificVideo;
+	}
+
+	public void setNotificVideo(String notificVideo) {
+		this.notificVideo = notificVideo;
+	}
+
+	public String getNotificEvent() {
+		return notificEvent;
+	}
+
+	public void setNotificEvent(String notificEvent) {
+		this.notificEvent = notificEvent;
+	}
+
+	public String getNotificAnnounce() {
+		return notificAnnounce;
+	}
+
+	public void setNotificAnnounce(String notificAnnounce) {
+		this.notificAnnounce = notificAnnounce;
+	}
+
+	public String getNotificTopic() {
+		return notificTopic;
+	}
+
+	public void setNotificTopic(String notificTopic) {
+		this.notificTopic = notificTopic;
+	}
+
+	public String getNotificComment() {
+		return notificComment;
+	}
+
+	public void setNotificComment(String notificComment) {
+		this.notificComment = notificComment;
 	}
 	
 }
