6e056af808ae289fe111cfded33647c6f1dc4a90
osvaldimar <osvaldimar1@gmail.com>
2016-10-02 10:18:50 -0300

    - Filtro para validar a sessao do membro nas seguintes jsp: "/page.jsp", "/home.jsp", "/index.jsp", "/" e redirecionar para pagina de login

--- PrjRedeSocial/src/com/iliketo/filter/FilterPageJSP.java
+++ PrjRedeSocial/src/com/iliketo/filter/FilterPageJSP.java
@@ -12,8 +12,6 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
-
 import HardCore.Request;
 import HardCore.RequireUser;
 import HardCore.Response;
