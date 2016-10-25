8711975a2f158f2f43cd24914bb1e4f299573e62
renatodanielss <renatodanielss@gmail.com>
2016-10-24 22:32:47 -0200

    Updated:   -Email.java

--- PrjRedeSocial/src/HardCore/Email.java
+++ PrjRedeSocial/src/HardCore/Email.java
@@ -169,25 +169,25 @@ public class Email {
 		if ((myconfig != null) && (! myconfig.get(db, "contact_form_mailserver").equals(""))) {
 			String[] mailserver = myconfig.get(db, "contact_form_mailserver").split(":");
 			if (mailserver.length >= 2) {
-				properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+				properties.setProperty("mail.smtp.host", "ns736.hostgator.com.br");
 				properties.setProperty("mail.smtp.port", "587");
 			} else {
-				properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+				properties.setProperty("mail.smtp.host", "ns736.hostgator.com.br");
 			}
 		} else {
-			properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+			properties.setProperty("mail.smtp.host", "ns736.hostgator.com.br");
 		}
 		
 		properties.put("mail.smtp.auth", "true");
 		properties.put("mail.smtp.starttls.enable", "true");
-		properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+		properties.put("mail.smtp.ssl.trust", "ns736.hostgator.com.br");
 
 
 		
 		
 		javax.mail.Session session = javax.mail.Session.getInstance(properties, new javax.mail.Authenticator() {
 				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication("contato.iliketo@gmail.com", "ahGhU5gFI13abW978Aim");
+					return new PasswordAuthentication("account-activation@iliketoo.com", "ahGhU5gFI13abW978Aim");
 				}
 		});
 		
